db35f8a6880e32cbe04855e5cf7fef2f4637354b
Ethan Phillips <ethansemail@yahoo.com>
2022-10-28 15:42:42 -0500

    UI Updates and added App Icon
    Added a text entry box for adding tasks, doesn't have a function just yet. I added a app icon and changed the color theme to a cool blue/mint color. This will be the main color throughout the app.

--- GrindTasks/MainPage.xaml.cs
+++ GrindTasks/MainPage.xaml.cs
@@ -14,11 +14,11 @@ public partial class MainPage : ContentPage
 		count++;
 
 		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
+			newTaskButton.Text = $"Clicked {count} time";
 		else
-			CounterBtn.Text = $"Clicked {count} times";
+			newTaskButton.Text = $"Clicked {count} times";
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
+		SemanticScreenReader.Announce(newTaskButton.Text);
 	}
 }
 
